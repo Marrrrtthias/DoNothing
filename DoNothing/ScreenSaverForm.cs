00a812f2dc2d23b4be953a9ad266e4fee4c25ab3
Matthias Gellrich <matthiasgellrich@gmail.com>
2020-12-22 17:38:58 +0100

    Add project files.

---
+++ DoNothing/ScreenSaverForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoNothing
+{
+    public partial class ScreenSaverForm : Form
+    {
+
+        private Point mouseLocation;
+
+        public ScreenSaverForm()
+        {
+            InitializeComponent();
+        }
+
+        public ScreenSaverForm(Rectangle Bounds)
+        {
+            InitializeComponent();
+            this.Bounds = Bounds;
+        }
+
+        private void ScreenSaverForm_Load(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+            TopMost = true;
+        }
+
+        private void ScreenSaverForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ScreenSaverForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ScreenSaverForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouseLocation.IsEmpty)
+            {
+                // Terminate if mouse is moved a significant distance
+                if (Math.Abs(mouseLocation.X - e.X) > 5 ||
+                    Math.Abs(mouseLocation.Y - e.Y) > 5)
+                    Application.Exit();
+            }
+
+            // Update current mouse location
+            mouseLocation = e.Location;
+        }
+    }
+}
